bdc81caab0d276d4781d8234109303b587252cba
Gymnophoria <buddercreeperman@gmail.com>
2018-12-04 22:25:46 -0800

    finish day 4

--- Day 4/partOne.js
+++ Day 4/partOne.js
@@ -21,23 +21,67 @@ let current = {}
 input.forEach(obj => {
     if (obj[6]) {
         current['guard'] = obj[6];
+        if (!guards[current.guard]) guards[current.guard] = {};
         if (!guards[current.guard].sleepTotal) guards[current.guard].sleepTotal = 0;
         if (!guards[current.guard].mins) {
             guards[current.guard].mins = {};
             for (let i = 0; i < 60; i++) 
                 guards[current.guard].mins[i] = 0;
         }
-        continue;
     }
 
     if (obj[5] === 'falls asleep') {
-        current['state'] = 'asleep';
+        current['time'] = obj.date;
+    }
+
+    if (obj[5] === 'wakes up') {
         let newSleep = obj.date - current['time'];
         guards[current.guard].sleepTotal += newSleep;
-        for (let i = current['time']; i < current['time'] + newSleep; i++) {
-            guards[current.guard].sleep[obj[4]]++;
+        for (let i = obj[4] - newSleep; i < obj[4]; i++) {
+            guards[current.guard].mins[i]++;
         }
-        current['time'] = obj.date;
     }
     
 });
+
+let guardsVals = Object.values(guards);
+let guardKeys = Object.keys(guards);
+let sleepiest = -1;
+let sleepiestSleepCount = 0;
+for (let i = 0; i < guardsVals.length; i++) {
+    if (guardsVals[i].sleepTotal > sleepiestSleepCount) {
+        sleepiest = guardKeys[i];
+        sleepiestSleepCount = guardsVals[i].sleepTotal;
+    }
+}
+
+let sleepiestMinute = -1;
+let sleepiestMinuteCount = -1;
+for (let i = 0; i < 60; i++) {
+    if (guards[sleepiest].mins[i] > sleepiestMinuteCount) {
+        sleepiestMinute = i;
+        sleepiestMinuteCount = guards[sleepiest].mins[i];
+    }
+}
+
+let mostMinute = -1;
+let mostMinuteTimes = -1;
+let mostMinuteGuard = -1;
+for (let i = 0; i < 60; i++) {
+    let currentTimes = -1;
+    let currentMostGuard = -1;
+    for (let j = 0; j < guardsVals.length; j++) {
+        if (guardsVals[j].mins[i] > currentTimes) {
+            currentMostGuard = guardKeys[j];
+            currentTimes = guardsVals[j].mins[i];
+        }
+    }
+    if (currentTimes > mostMinuteTimes) {
+        mostMinute = i;
+        mostMinuteTimes = currentTimes;
+        mostMinuteGuard = currentMostGuard;
+    }
+}
+
+console.log(`Sleepiest guard is guard #${sleepiest} with a crazy ${sleepiestSleepCount} minutes total and sleepiest minute ${sleepiestMinute} with multiplied answer of ${sleepiest * sleepiestMinute}.`);
+console.log(`Guard who sleeps the most at one given minute is #${mostMinuteGuard} on minute ${mostMinute} (${guards[mostMinuteGuard].mins[mostMinute]} times) with mult ${mostMinute * mostMinuteGuard}.`);
